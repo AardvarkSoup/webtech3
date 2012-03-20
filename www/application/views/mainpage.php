8b1e7a91db390945f9770e3c2cf964a1912092b3
Tom Tervoort <tomtervoort@gmail.com>
2012-03-20 23:28:33 +0100

    Alvast even een hoofdpagina aangemaakt.

---
+++ www/application/views/mainpage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Yet untitled dating site</title>
+</head>
+<body>
+
+<div>
+
+<p>Main page here☃</p>
+
+</div>
+
+</body>
+</html>

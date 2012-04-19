b9862f00af4c53444366a8ecea749db657bd9806
Tom Tervoort <tomervoort@gmail.com>
2012-04-19 22:24:51 +0200

    Klein bugje in matching-code opgelost.

--- www/application/models/matching.php
+++ www/application/models/matching.php
@@ -10,7 +10,7 @@ class Matching extends CI_Model
         {
             if(!isset($arrayA[$val['brandName']]))
             {
-                ++$result[];
+                ++$result;
             }
         }
         
